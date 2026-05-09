7d2c35fa320fa6d337b4a6d04851a75c263af1e6
bbslama <ben.slama.bechir@gmail.com>
2026-05-09 17:36:22 +0200

    generate component

--- angular-crud-app/src/app/components/user-manager/user-manager.ts
+++ angular-crud-app/src/app/components/user-manager/user-manager.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { User, UserService } from '../../services/users';
 
 @Component({
   selector: 'app-user-manager',
-  imports: [],
-  templateUrl: './user-manager.html',
-  styleUrl: './user-manager.scss',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './user-manager.component.html'
 })
-export class UserManager {}
+export class UserManagerComponent implements OnInit {
+  users: User[] = [];
+  currentUser: User = { name: '', email: '' };
+  isEdit = false;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService.getUsers().subscribe(data => this.users = data);
+  }
+
+  saveUser() {
+    if (this.isEdit && this.currentUser.id) {
+      this.userService.updateUser(this.currentUser.id, this.currentUser)
+        .subscribe(() => { this.resetForm(); this.loadUsers(); });
+    } else {
+      this.userService.createUser(this.currentUser)
+        .subscribe(() => { this.resetForm(); this.loadUsers(); });
+    }
+  }
+
+  editUser(user: User) {
+    this.currentUser = { ...user };
+    this.isEdit = true;
+  }
+
+  deleteUser(id: number | undefined) {
+    if (id) {
+      this.userService.deleteUser(id).subscribe(() => this.loadUsers());
+    }
+  }
+
+  resetForm() {
+    this.currentUser = { name: '', email: '' };
+    this.isEdit = false;
+  }
+}
+
